7e7dd159b15c5cc112834e1fe8082f0d53a7eb1a
xelawe <xelawe@users.noreply.github.com>
2016-11-26 13:08:11 +0100

    first setup

--- esp8266_snowfall.ino
+++ esp8266_snowfall.ino
@@ -1,9 +1,25 @@
+#define serdebug
+#ifdef serdebug
+#define DebugPrint(...) {  Serial.print(__VA_ARGS__); }
+#define DebugPrintln(...) {  Serial.println(__VA_ARGS__); }
+#else
+#define DebugPrint(...) { }
+#define DebugPrintln(...) { }
+#endif
+
+#include "tools_wifi.h"
+#include "ota_tool.h"
+
 void setup() {
   // put your setup code here, to run once:
 
+  wifi_init("NightLight");
+
+  init_ota("SnowFall");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  check_ota();
 }
